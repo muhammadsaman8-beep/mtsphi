448516691b07104cf8f1918d439ee2a38e64538f
muhammadsaman8-beep <muhammadsaman8@madrasah.kemenag.go.id>
2026-07-15 19:05:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     viteReact(),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
 });
